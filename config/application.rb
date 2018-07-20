85cad031d6b08e2a6e9b3313e6821e8ec6ec8dc0
kazuya-hachisuka <kzy8sk@gmail.com>
2018-07-20 10:13:28 +0000

    Rails時刻をJST設定に変更

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module QRTimeCard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # 時刻をJSTに変更
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
