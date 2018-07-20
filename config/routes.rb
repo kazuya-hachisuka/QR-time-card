bbbdd66c5ba92f66f0e856029f207cf696fdd680
kazuya-hachisuka <kzy8sk@gmail.com>
2018-07-20 08:22:38 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get 'cameras/show' => 'cameras#show' #instascan モバイル用
 
   get 'cameras/index' => 'cameras#index' #jsQR PC用
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
