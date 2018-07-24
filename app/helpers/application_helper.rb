26342a85553301ffdd3bafcb7941de9b87ec70fe
kazuya-hachisuka <kzy8sk@gmail.com>
2018-07-24 10:02:25 +0000

    労働時間 休憩時間 算出機能OK ransackで絞り込みでも算出OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,27 @@ module ApplicationHelper
   def qrcode_tag(text, options = {})
     ::RQRCode::QRCode.new("https://github.com/whomwah/rqrcode/blob/master/README.md").as_svg(options).html_safe
   end
+
+  def totalwork(works)
+		totalwork = 0
+    works.each do |workday|
+    	if workday.out.present?
+	    sec = (workday.out.in_time_zone - workday.in.in_time_zone)
+	    totalwork += sec
+    	end
+    end
+    return totalwork
+	end
+
+	# def totalbreak(breaks)
+	# 	totalbreak = 0
+	# 	breaks.each do |break_time|
+	# 		unless break_time.break_out.blank?
+	# 			off_time = (break_time.break_out.in_time_zone - break_time.break_in.in_time_zone)
+	# 			totalbreak += off_time
+	# 			end
+	# 		end
+	# 		return totalbreak
+	# end
+
 end
