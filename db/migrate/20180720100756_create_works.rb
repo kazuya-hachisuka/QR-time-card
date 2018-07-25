b1621101cb81f367e710d77227d2d5359ca2cd9d
kazuya-hachisuka <kzy8sk@gmail.com>
2018-07-25 07:39:32 +0000

    add to cocoon 勤怠、休憩ともに保存OK

--- db/migrate/20180720100756_create_works.rb
+++ db/migrate/20180720100756_create_works.rb
@@ -1,7 +1,7 @@
 class CreateWorks < ActiveRecord::Migration[5.1]
   def change
     create_table :works do |t|
-      t.datetime :in
+      t.datetime :in, null: false
       t.datetime :out
       t.integer :staff_id, null: false, index: true
 
